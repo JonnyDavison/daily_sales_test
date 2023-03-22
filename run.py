ff3ebd5b81ea58b85351e99dca072f7fba7caec1
JonnyDavison <jonnydavison@icloud.com>
2023-03-22 14:28:23 +0000

    Add float to sales_data & complete total_sale function

--- run.py
+++ run.py
@@ -17,45 +17,63 @@ def sales_data():
     """
     Get end of day sales totals by department
     """
-    print('Please enter sales by department')
+    print('Please enter todays sales by department')
     print('Figures should be in the format requested')
     print('Example dd/mm/yyyy or 1234.56 \n')
 
-    returned_date = input("Please enter todays Date here dd/mm/yyyy: ")
+    returned_date = (input("Please enter todays Date here dd/mm/yyyy: "))
     print(f"Date provided: {returned_date} \n")
-
-    returned_hot_food = input("Please enter todays hot food sales format: 1234.56: ")
+    
+    returned_hot_food = float(input("Please enter hot food sales eg 12.34: "))
     print(f"Hot food sales where {returned_hot_food}\n")
 
-    returned_cakes = input("Please enter todays cake sales, format: 1234.56: ")
+    returned_cakes = float(input("Please enter cake sales, eg 12.34: "))
     print(f"Cake sales where {returned_cakes}\n")
 
-    returned_bread = input("Please enter todays bread sales, format: 1234.56: ")
+    returned_bread = float(input("Please enter bread sales, eg 12.34: "))
     print(f"Bread sales where {returned_bread}\n")
 
-    returned_coffee = input("Please enter todays coffee sales, format: 1234.56: ")
+    returned_coffee = float(input("Please enter coffee sales, eg 12.34: "))
     print(f"Coffee sales where {returned_coffee}\n")
 
-    returned_0_vat = input("Please enter todays 0% VAT sales, format: 1234.56: ")
+    returned_0_vat = float(input("Please enter 0% VAT sales, eg 12.34: "))
     print(f"0% VAT sales where {returned_0_vat}\n")
 
-    returned_wine = input("Please enter todays wine sales, format: 1234.56: ")
+    returned_wine = float(input("Please enter wine sales, eg 12.34: "))
     print(f"Wine sales where {returned_wine}\n")
 
-    returned_chilled = input("Please enter todays Chilled goods sales, format: 1234.56: ")
+    returned_chilled = float(input("Please enter Chilled sales, eg 12.34: "))
     print(f"Chilled goods sales where {returned_chilled}\n")
 
-    returned_minerals = input("Please enter todays mineral sales, format: 1234.56: ")
+    returned_minerals = float(input("Please enter mineral sales, eg 12.34: "))
     print(f"Minerals sales where {returned_minerals}\n")
-
-    returned_sweets = input("Please enter todays sweet sales, format: 1234.56: ")
+   
+    returned_sweets = float(input("Please enter sweet sales, eg 12.34: "))
     print(f"Sweets sales where {returned_sweets}\n")
 
-    returned_chocolate = input("Please enter todays chocolate sales, format: 1234.56: ")
-    print(f"chocolate sales where {returned_chocolate}\n")
+    returned_choc = float(input("Please enter chocolate sales, eg 12.34: "))
+    print(f"chocolate sales where {returned_choc}\n")
 
-    total_sales = (2*8)
-    print(f'Thank you todays total sales {total_sales}\n')
+    total_sales = (returned_hot_food + returned_cakes + returned_bread
+                   + returned_coffee + returned_0_vat + returned_wine 
+                   + returned_chilled + returned_minerals + returned_sweets 
+                   + returned_choc)
+    print(f'Thank you, todays total sales {total_sales}\n')
 
 
-sales_data() 
+def validate_sales(values):
+    """
+    Converts string to float 
+    """
+    try:
+        float(values)
+        print(f"The value you entered was {values}")
+        raise ValueError(
+                print(f"Thank you, {values} \n ")
+            )
+
+    except ValueError as e:
+        print(f'Invalid data {e}, please try again')    
+
+
+sales_data()
